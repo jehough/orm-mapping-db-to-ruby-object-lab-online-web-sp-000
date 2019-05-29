0354054c36a816007daf9335ba8c1d2f2b918e78
Jim Hough <teachlife@outlook.com>
2019-05-29 02:04:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,9 +70,16 @@ class Student
     self.first_X_students_in_grade_10(1)[0]
   end
 
+  def self.all_students_in_grade_X (num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
 
-
-
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def save
     sql = <<-SQL
