f4f77a6f2419786fdc8b43e97cf32b48e972da2b
Jim Hough <teachlife@outlook.com>
2019-05-29 01:36:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,10 @@ class Student
 
   def self.all
     sql = 'SELECT * FROM students'
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
